1e3b54430f240277b85d6af467f78db265731261
crastilo <139198723+crastilo@users.noreply.github.com>
2023-07-21 23:00:06 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "https://trck.securedcdsnt.com/smartlink/?a=159961&sm=18397&co=209017&mt=28&s1=rai_y"
+const offerLink = "https://xoabnm.datlngllfe.net/?utm_source=da57dc555e50572d&s1=152896&s2=1899728&s3=Rahid_vi&j1=1"
